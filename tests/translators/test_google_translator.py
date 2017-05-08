b921d458efdae44964ae3e020d8fd1e7b7bf389d
Johan de Jager <johan.dejager@live.nl>
2017-05-08 11:36:06 +0200

    Updated Mircea's test to use assertEquals and the new translator API'

--- tests/translators/test_google_translator.py
+++ tests/translators/test_google_translator.py
@@ -81,21 +81,21 @@ class TestGoogleTranslator(TestCase):
 
         self.assertNotIn('&#39;', translation)
 
-<<<<<<< HEAD
     def testTwoWords(self):
-        translation = self.goog.ca_translate(u'Wadenkrampf erlitten', 'de', 'en',
-                                            u'Offensichtlich hatte eine der Athletinnen während der Kür einen',
-                                             u' genau .')
-        assert "leg cramp suffered" == translation
+        translation = self.translators['de-en'].ca_translate(
+                                    before_context=u'Offensichtlich hatte eine der Athletinnen während der Kür einen',
+                                    query=u'Wadenkrampf erlitten',
+                                    after_context=u' genau .')
+
+        self.assertEquals("leg cramp suffered", translation)
 
         # However, if the sentence ends after our looked up word, we don't get the full translation!
-        translation = self.goog.ca_translate(u'Wadenkrampf erlitten', 'de', 'en',
-                                            u'Offensichtlich hatte eine der Athletinnen während der Kür einen',
-                                             u' .')
-        assert "leg cramp suffered" == translation
 
-=======
+        translation = self.translators['de-en'].ca_translate(
+                                query=u'Wadenkrampf erlitten',
+                                before_context=u'Offensichtlich hatte eine der Athletinnen während der Kür einen',
+                                after_context=u' .')
+        self.assertEquals("leg cramp suffered", translation)
 
 if __name__ == '__main__':
     unittest.main()
->>>>>>> bb3b6db30db629af93bc0293331f0e9959519802
