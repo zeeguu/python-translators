9ee1ba1d8b0008af0d54eaf7124369739185fdbb
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-17 12:55:06 +0100

    added more complete service names for google translators fixed priorities for google with context and google without context disabled microsoft for now… they’re ten times slower than google…

--- tests/translators/test_best_effort_translator.py
+++ tests/translators/test_best_effort_translator.py
@@ -1,13 +1,14 @@
 from unittest import TestCase
 
 from python_translators.translation_query import TranslationQuery
-from python_translators.translators.best_effort_translator import DummyBestEffortTranslator
+from python_translators.translators.best_effort_translator import DummyBestEffortTranslator, BestEffortTranslator
 
 
 class TestBestEffortTranslator(TestCase):
 
     def setUp(self):
-        self.translator = DummyBestEffortTranslator(source_language='es', target_language='en')
+        self.translator = DummyBestEffortTranslator(source_language='de', target_language='en')
+        self.bet = BestEffortTranslator(source_language='de', target_language='en')
 
     def testEnsureDifferentWordThanOriginalReturnedIfPossible(self):
         response = self.translator.translate(TranslationQuery(
@@ -16,3 +17,14 @@ class TestBestEffortTranslator(TestCase):
         ))
 
         self.assertNotEqual(response.translations[0]['translation'], 'ada')
+
+    def testEnsureContextualHasPriority(self):
+
+        response = self.bet.translate(TranslationQuery(
+            before_context='Hunderte Züge und Dutzende Flüge wurden ',
+            query='gestrichen',
+            after_context=' .',
+            max_translations=3
+        ))
+
+        assert (response.translations[0]['translation'] == "canceled")
