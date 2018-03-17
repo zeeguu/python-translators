41a5fcc796fd902ca6c4edc8bc54577362a6fc38
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-17 11:57:37 +0100

    ordering responses by quality; decreasing quality for translations that equal the query; better testing of BET

--- python_translators/translators/duplicate_translator.py
+++ python_translators/translators/duplicate_translator.py
@@ -4,13 +4,15 @@ from python_translators.translation_query import TranslationQuery
 from python_translators.translation_response import TranslationResponse
 from python_translators.translation_costs import TranslationCosts
 
+import requests
 
-class ReverseTranslator(Translator):
+
+class DuplicateTranslator(Translator):
 
     def __init__(self, source_language: str, target_language: str, translator_name: str = 'Reverse',
                  quality: int = '10',
                  service_name: str = 'Reverse') -> None:
-        super(ReverseTranslator, self).__init__(source_language, target_language, translator_name, quality,
+        super(DuplicateTranslator, self).__init__(source_language, target_language, translator_name, quality,
                                                 service_name)
 
     def _translate(self, query: TranslationQuery) -> TranslationResponse:
@@ -20,15 +22,13 @@ class ReverseTranslator(Translator):
         :return: 
         """
 
-        simple_reverse = self.make_translation(query.query[::-1])
-        title_reverse = self.make_translation(query.query[::-1].title())
-        upper_reverse = self.make_translation(query.query[::-1].upper())
-        translations = [simple_reverse, title_reverse, upper_reverse][:query.max_translations]
+        duplicate = self.make_translation(query.query+query.query)
+        translations = [duplicate]
 
         return TranslationResponse(
             translations=translations,
             costs=TranslationCosts(
-                money=0  # API is free
+                money=0  # no costs for this
             )
         )
 
---
+++ tests/translators/test_best_effort_translator.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from translation_query import TranslationQuery
+from python_translators.translators.best_effort_translator import DummyBestEffortTranslator
+
+
+class TestReverseTranslator(TestCase):
+
+    def setUp(self):
+        self.translator = DummyBestEffortTranslator(source_language='es', target_language='en')
+
+    def testEnsureDifferentWordThanOriginalReturnedIfPossible(self):
+        response = self.translator.translate(TranslationQuery(
+            query="ada",
+            max_translations=1
+        ))
+
+        self.assertNotEqual(response.translations[0]['translation'], 'ada')
