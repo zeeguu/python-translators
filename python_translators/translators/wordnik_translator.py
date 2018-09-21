5ca1a5984b12c64a9f58a1346935a84d09e9c2ef
Mircea Lungu <mircea.lungu@gmail.com>
2018-09-21 12:25:41 +0200

    towards following up on related words

--- python_translators/translators/wordnik_translator.py
+++ python_translators/translators/wordnik_translator.py
@@ -6,8 +6,12 @@ from python_translators.translation_costs import TranslationCosts
 
 from wordnik import swagger, WordApi
 
+import nltk
+
 API_URL = 'http://api.wordnik.com/v4'
 MAX_WORDS_IN_DEFINITION = 16
+
+META_DEFINITION_PREFIXES = ["Present participle of", "Simple past tense and past participle of"]
 
 
 class WordnikTranslator(Translator):
@@ -36,30 +40,59 @@ class WordnikTranslator(Translator):
         :param query: 
         :return: 
         """
-
-        if self.source_language != "en" and self.target_language != "en":
-            return []
+        full_sentence = query.before_context + query.query + query.after_context
 
         response = self.word_api.getDefinitions(query.query)
+        if not response:
+            response = []
 
-        # Extract the translations (thanks @SAMSUNG)
         translations = []
         quality = int(self.get_quality())
 
-        for definition in response[0:query.max_translations]:
-            try:
-                quality -= 1
-                if len(definition.text.split(" ")) < MAX_WORDS_IN_DEFINITION:
-                    translations.append(self.make_translation(definition.text, str(quality)))
-            except KeyError:
-                pass
+        for d in response[:query.max_translations]:
+            quality -= 1
 
-        return TranslationResponse(
+            definition = self.definition_without_example_and_without_see_synonims(d)
+            if self.not_too_long(definition):
+                translations.append(self.make_translation(definition, quality))
+
+            meta_defined_word = self.is_meta_definition(definition)
+            if meta_defined_word:
+                response2 = self.word_api.getDefinitions(meta_defined_word)
+                for d2 in response2[:query.max_translations]:
+                    d2clean = self.definition_without_example_and_without_see_synonims(d2)
+                    if self.not_too_long(d2clean):
+                        translations.append(self.make_translation(meta_defined_word+": "+d2clean, quality))
+
+
+        # if we don't know the translation, just parrot back the question
+        if not translations:
+            translations.append(self.make_translation(query.query, quality))
+
+        rez = TranslationResponse(
             translations=translations,
             costs=TranslationCosts(
                 money=0  # API is free
             )
         )
+
+        return rez
+
+    def not_too_long(self, definition):
+        return len(definition.split(" ")) < MAX_WORDS_IN_DEFINITION
+
+    def is_meta_definition(self, definition:str):
+        for prefix in META_DEFINITION_PREFIXES:
+            if prefix in definition:
+                return definition.split(prefix)[1].strip(" ,.;")
+        return None
+
+    def definition_without_example_and_without_see_synonims(self, definition):
+
+        rez = definition.text.split(":")[0]
+        rez = rez.split(".")[0]
+        return rez
+
 
     def compute_money_costs(self, query: TranslationQuery) -> float:
         return .0
