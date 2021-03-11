a1199fc905e6f64a9362b5163d05c8401409d24a
Mircea Lungu <mircea.lungu@gmail.com>
2021-03-11 14:59:06 +0100

    logging to stdout by default

--- python_translators/__init__.py
+++ python_translators/__init__.py
@@ -1,20 +1,27 @@
 import logging
+import sys
 
-logger = logging.getLogger('python_translators')
+logger = logging.getLogger("python_translators")
 logger.setLevel(logging.DEBUG)
 
 # General logging tracking ALL logs
-fh = logging.FileHandler('translators.log', encoding = "UTF-8")
+# to log to file use the following line
+# fh = logging.FileHandler('translators.log', encoding = "UTF-8")
+fh = logging.StreamHandler(sys.stdout)
+
 fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 fh.setFormatter(formatter)
 
 
-fh2 = logging.FileHandler('errors.translators.log')
+# use the following line for logging to file
+# fh2 = logging.FileHandler("errors.translators.log")
+fh2 = logging.StreamHandler(sys.stdout)
+
 fh2.setLevel(logging.ERROR)
 fh2.setFormatter(formatter)
 
 logger.addHandler(fh2)
 logger.addHandler(fh)
 
-logger.info('Logger initialized.')
+logger.info("Logger initialized.")
