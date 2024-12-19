67caaadd551733f6c12fe1226dd9c8b01ef34571
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-12-19 13:24:31 +0100

    removed some wordnik tests - it's behavior has changed in the meantime.

--- tests/translators/test_best_effort_translator.py
+++ tests/translators/test_best_effort_translator.py
@@ -1,58 +1,68 @@
 from unittest import TestCase
 
 from python_translators.translation_query import TranslationQuery
-from python_translators.translators.best_effort_translator import DummyBestEffortTranslator, BestEffortTranslator
+from python_translators.translators.best_effort_translator import (
+    DummyBestEffortTranslator,
+    BestEffortTranslator,
+)
 
 
 class TestBestEffortTranslator(TestCase):
 
     def setUp(self):
-        self.translator = DummyBestEffortTranslator(source_language='de', target_language='en')
-        self.bet = BestEffortTranslator(source_language='de', target_language='en')
+        self.translator = DummyBestEffortTranslator(
+            source_language="de", target_language="en"
+        )
+        self.bet = BestEffortTranslator(source_language="de", target_language="en")
 
     def testEnsureDifferentWordThanOriginalReturnedIfPossible(self):
-        response = self.translator.translate(TranslationQuery(
-            query="ada",
-            max_translations=1
-        ))
+        response = self.translator.translate(
+            TranslationQuery(query="ada", max_translations=1)
+        )
 
-        self.assertNotEqual(response.translations[0]['translation'], 'ada')
+        self.assertNotEqual(response.translations[0]["translation"], "ada")
 
     def testEnsureContextualHasPriority(self):
-        response = self.bet.translate(TranslationQuery(
-            before_context='Hunderte Züge und Dutzende Flüge wurden ',
-            query='gestrichen',
-            after_context=' .',
-            max_translations=3
-        ))
+        response = self.bet.translate(
+            TranslationQuery(
+                before_context="Hunderte Züge und Dutzende Flüge wurden ",
+                query="gestrichen",
+                after_context=" .",
+                max_translations=3,
+            )
+        )
 
-        assert (response.translations[0]['translation'] == "canceled")
+        assert response.translations[0]["translation"] == "cancelled"
 
     def test_issue_20__avoidance_of_suspiciously_long_translations(self):
         # source of example:
         # http://www.spiegel.de/lebenundlernen/job/equal-pay-day-warum-die-deutschen-nicht-ueber-geld-reden-a-1198494.html
-        response = self.bet.translate(TranslationQuery(
-            before_context='Der amerikanische Traum wurzelt in dem Glauben, dass der ',
-            query='Einzelne',
-            after_context=' es vom Tellerwäscher zum Milliardär schaffen kann.',
-            max_translations=3
-        ))
+        response = self.bet.translate(
+            TranslationQuery(
+                before_context="Der amerikanische Traum wurzelt in dem Glauben, dass der ",
+                query="Einzelne",
+                after_context=" es vom Tellerwäscher zum Milliardär schaffen kann.",
+                max_translations=3,
+            )
+        )
 
         # this works only after we 'half' the importance of the Google -- with context which
         # translated more than the actual query normally
         # [{'translation': 'individual', 'service_name': 'Microsoft - with context', 'quality': 75},
         # {'translation': 'Single', 'service_name': 'Google - without context', 'quality': 70},
         # {'translation': 'individual can make', 'quality': 40, 'service_name': 'Google - with context'}]
-        assert (response.translations[0]['translation'] in ["individual", "individuals"])
+        assert response.translations[0]["translation"] in ["individual", "individuals"]
 
     def test_issue_41__avoidance_of_empty_strings(self):
         # source of example:
         # http://www.spiegel.de/panorama/justiz/indiana-bankraeuber-kam-mit-dem-taxi-a-1198724.html
-        response = self.bet.translate(TranslationQuery(
-            before_context='Nach der Fahrt zu der Bank ',
-            query='schob',
-            after_context=' er dem Angestellten an der Kasse lediglich einen Zettel zu: "Dies ist ein Überfall.',
-            max_translations=3
-        ))
+        response = self.bet.translate(
+            TranslationQuery(
+                before_context="Nach der Fahrt zu der Bank ",
+                query="schob",
+                after_context=' er dem Angestellten an der Kasse lediglich einen Zettel zu: "Dies ist ein Überfall.',
+                max_translations=3,
+            )
+        )
 
-        assert (response.translations[0]['translation'] != "")
+        assert response.translations[0]["translation"] != ""
--- tests/translators/test_wordnik_translator.py
+++ tests/translators/test_wordnik_translator.py
@@ -9,55 +9,19 @@ from python_translators.config import get_key_from_config
 class TestWordnikTranslator(TestCase):
 
     def setUp(self):
-        self.translator = WordnikTranslator(source_language='es',
-                                            target_language='en',
-                                            key=get_key_from_config('WORDNIK_API_KEY'))
-
-    def testNumberOfTranslationsWorks(self):
-        response = self.translator.translate(TranslationQuery(
-            query="conjunction",
-            max_translations=5
-        ))
-
-        assert 2 <= len(response.translations)
+        self.translator = WordnikTranslator(
+            source_language="es",
+            target_language="en",
+            key=get_key_from_config("WORDNIK_API_KEY"),
+        )
 
     def testUppercaseMatters(self):
-        response = self.translator.translate(TranslationQuery(
-            query="March",
-            max_translations=5
-        ))
-        response2 = self.translator.translate(TranslationQuery(
-            query="march",
-            max_translations=5
-        ))
+        response = self.translator.translate(
+            TranslationQuery(query="March", max_translations=5)
+        )
+        response2 = self.translator.translate(
+            TranslationQuery(query="march", max_translations=5)
+        )
 
         # The first is the month; the second is the verb
         assert response.translations != response2.translations
-
-    def testWordnikShouldLowercaseByItself(self):
-        response = self.translator.translate(TranslationQuery(
-            query="Conjunction",
-            max_translations=5
-        ))
-
-        self.assertFalse(response.translations == [])
-
-    def testQuotedWord(self):
-        response1 = self.translator.translate(TranslationQuery(
-            query="'insincere'",
-            max_translations=5
-        ))
-
-        response2 = self.translator.translate(TranslationQuery(
-            query="insincere",
-            max_translations=5
-        ))
-
-        response3 = self.translator.translate(TranslationQuery(
-            query=''"insincere"'',
-            max_translations=5
-        ))
-
-        assert (len(response1.translations) ==
-                len(response2.translations) ==
-                len(response3.translations))
