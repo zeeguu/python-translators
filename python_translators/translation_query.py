499183fedd3230b67718a6d0a9c21ac088c0f38d
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-12-19 12:48:02 +0100

    Using a regex to find an actual word to translate in context, and not the first occurrence of a string. Removed unused methods

--- python_translators/translation_query.py
+++ python_translators/translation_query.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 
 class TranslationBudget(object):
@@ -14,12 +15,14 @@ class TranslationBudget(object):
 
 
 class TranslationQuery(object):
-    def __init__(self,
-                 query: str,
-                 before_context: str = '',
-                 after_context: str = '',
-                 max_translations: int = 1,
-                 budget: TranslationBudget = None):
+    def __init__(
+        self,
+        query: str,
+        before_context: str = "",
+        after_context: str = "",
+        max_translations: int = 1,
+        budget: TranslationBudget = None,
+    ):
         self.query = query
         self.before_context = before_context
         self.after_context = after_context
@@ -36,59 +39,34 @@ class TranslationQuery(object):
         return self.budget.is_unconstrained()
 
     @classmethod
-    def for_word_occurrence(cls, query: str, context: str, word_index_for_query: int, max_translations: int = 1):
-
+    def for_word_occurrence(
+        cls,
+        query: str,
+        context: str,
+        word_index_for_query: int,
+        max_translations: int = 1,
+    ):
         """
-            Useful when context is given as a single paragraph, but it's known which occurrence of
-            the query is interesting (in the situation where there's multiple occurrences of query
-            in the context)
+        Useful when context is given as a single paragraph, but it's known which occurrence of
+        the query is interesting (in the situation where there's multiple occurrences of query
+        in the context)
+
+        Note: the current implementation does not use the word_index_for_query
         """
-        result = context.split(query, word_index_for_query)
-        before_context = after_context = ''
 
-        if len(result) > 1:
-            before_context, after_context = result[0], result[1]
+        result = re.search(r"\b" + re.escape(query) + r"\b", context).span()
 
-        return cls(query, before_context=before_context, after_context=after_context, max_translations=max_translations)
+        before_context = context[0 : result[0]]
+        after_context = context[result[1] + len(query) :]
+
+        return cls(
+            query,
+            before_context=before_context,
+            after_context=after_context,
+            max_translations=max_translations,
+        )
 
     @classmethod
-    def for_word_at_index(cls, query: str, context: str, char_index: int, max_translations: int = 1):
-
-        """
-            Convenient when context is given as a whole.
-            The char_index insures that in case of multiple occurrences of the query in the context
-             the right one is being translated.
-        """
-        before_context, after_context = context[:char_index], context[:char_index + len(query)]
-        return cls(query, before_context=before_context, after_context=after_context, max_translations=max_translations)
-
-    @classmethod
-    def one_context_and_word_index(cls, query: str, context: str, word_index_for_query: int, max_translations: int = 1):
-
-        """
-
-            Useful when context is given as a single paragraph, but it's known which occurrence of
-            the query is interesting (in the situation where there's multiple occurrences of query
-            in the context)
-
-        """
-        before_context, after_context = context.split(query, word_index_for_query)
-        return cls(query, before_context=before_context, after_context=after_context, max_translations=max_translations)
-
-    @classmethod
-    def one_context_and_char_index(cls, query: str, context: str, char_index: int, max_translations: int = 1):
-
-        """
-
-            Convenient when context is given as a whole.
-
-            The char_index insures that in case of multiple occurrences of the query in the context
-             the right one is being translated.
-
-        """
-        before_context, after_context = context[:char_index], context[:char_index + len(query)]
-        return cls(query, before_context=before_context, after_context=after_context, max_translations=max_translations)
-
     def is_context_aware_request(self):
         return self.before_context or self.after_context
 
